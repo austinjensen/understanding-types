cb522c11efa84bdee7164610a274a9b0a5c6e06c
Austin Jensen <ajjensen1329@gmial.com>
2018-04-16 19:48:36 -0600

    first commit

---
+++ unstan-types.js
@@ -0,0 +1,19 @@
+
+
+
+const fn= 'Austin ' 
+const ln = 'Jensen'
+ let name = fn + ln
+
+
+const A = 7
+const B = 19
+const calc = A + B
+
+
+
+
+let uethree = 3
+let uestring = ' unexpected'
+const unexpected = uethree + uestring
+
